efd01974dbbe3d1d06560812073157a07c4befef
1kuxyz <40167317+1kuxyz@users.noreply.github.com>
2021-08-08 02:40:19 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,20 +1,20 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'ef43d1756d1c4d7cb49c8bc96dec3b13',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: "1库xyz's Blog",
+  domain: 'notion.1ku.xyz',
+  author: '1库xyz',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  description: '风吹稻花香，小河水流淌',
+  socialImageTitle: 'Haha socialImageTitle',
+  socialImageSubtitle: 'Haha Hello World! 👋',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
